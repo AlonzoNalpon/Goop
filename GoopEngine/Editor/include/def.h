9a8996fcef7cec7dd2492789ab7325ec3773b5cf
BearCustomerService <a.nalpon@digipen.edu>
2023-09-06 09:27:16 +0800

    Project settings updated

---
+++ GoopEngine/Editor/include/def.h
@@ -0,0 +1,10 @@
+#ifndef DEF_H
+#define DEF_H
+
+using ubyte   = unsigned char;
+using uint32  = unsigned int;
+using i32     = int;
+using f64     = double;
+using f32     = float;
+using wchar   = wchar_t;
+#endif
