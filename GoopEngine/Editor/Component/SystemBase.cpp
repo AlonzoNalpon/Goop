986b76bda0bb48c1994a69aaedd29b0fac9564c9
BryanWongCK <bryanwongchinkit@gmail.com>
2023-09-06 12:35:08 +0800

    staging system files
    TODO: create component manager & system manager; follow design on ipad

---
+++ GoopEngine/Editor/Component/SystemBase.cpp
@@ -0,0 +1,52 @@
+#include "SystemBase.h"
+
+using namespace GE::ECS;
+
+SystemBase::SystemBase()
+{
+}
+
+SystemBase::~SystemBase()
+{
+}
+
+void SystemBase::Awake()
+{
+}
+
+void SystemBase::Start()
+{
+}
+
+void SystemBase::Update()
+{
+}
+
+void SystemBase::LateUpdate()
+{
+}
+
+void SystemBase::FixedUpdate()
+{
+}
+
+void SystemBase::OnDisable()
+{
+}
+
+void SystemBase::OnEnable()
+{
+}
+
+void SystemBase::OnDestroy()
+{
+}
+
+void SystemBase::SetActive(bool active)
+{
+}
+
+const bool& SystemBase::GetActive() const
+{
+
+}
