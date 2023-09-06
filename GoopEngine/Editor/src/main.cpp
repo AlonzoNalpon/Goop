e066551bd216d02e7fdeb53d798d90a8696325bc
BearCustomerService <a.nalpon@digipen.edu>
2023-09-06 09:52:59 +0800

    precompiled headers added

--- GoopEngine/Editor/src/main.cpp
+++ GoopEngine/Editor/src/main.cpp
@@ -1,15 +1,20 @@
 #include <GL/glew.h> // for access to OpenGL API declarations 
-#include<GLFW/glfw3.h>
+//#include <GLFW/glfw3.h>
 #include <iostream>
+#include "pch.h"
 
 int main(int argc, char* argv[])
 {
-  if (!glfwInit())
-  {
-    // Initialization failed!
-    std::cout << "Failed to initialize GLFW! Exiting now..." << std::endl;
-    std::exit(EXIT_FAILURE);
-  }
+  //if (!glfwInit())
+  //{
+  //  // Initialization failed!
+  //  std::cout << "Failed to initialize GLFW! Exiting now..." << std::endl;
+  //  std::exit(EXIT_FAILURE);
+  //}
+
+  //Goop::Window window(100, 100, L"WHAT");
+  //window.CreateOGLWindow();
+
   std::cout << "Hello World!" << std::endl;
 
   return 1;
