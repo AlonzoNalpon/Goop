83a95cb4076c114652fc78d1ebda20b776518098
BryanWongCK <bryanwongchinkit@gmail.com>
2023-09-08 15:42:44 +0800

    Added comments

--- GoopEngine/Editor/ECS/Entity/Entity.h
+++ GoopEngine/Editor/ECS/Entity/Entity.h
@@ -9,6 +9,8 @@ namespace GE
 		constexpr size_t MAX_COMPONENTS{ 32 };
 
 		using Entity = unsigned int;
+		// This is just a bitfield to track unique components
+		// Functionally the same as ComponentType
 		using ComponentSignature = std::bitset<MAX_COMPONENTS>;
 	}
 }
