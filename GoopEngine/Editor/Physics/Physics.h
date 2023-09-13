2fd904d8e2f22102dd28d9a7b51a1c653f9ba852
jiaaxenn <chrisxphuaxje@gmail.com>
2023-09-13 10:40:04 +0800

    Submitted new files for physics

--- GoopEngine/Editor/Physics/Physics.h
+++ GoopEngine/Editor/Physics/Physics.h
@@ -1,21 +1,7 @@
-#include "../ECS/System/System.h"
-#include "../Math/GEM.h"
-#include "Gravity.h"
-#include "Position.h"
-#include "Velocity.h"
-
-using vec2 = GE::Math::dVec2;
-
 namespace GE
 {
 	namespace Physics
 	{
-		class PhysSys : public GE::ECS::System
-		{
-		public:
 
-		private:
-			
-		};
 	}
 }
