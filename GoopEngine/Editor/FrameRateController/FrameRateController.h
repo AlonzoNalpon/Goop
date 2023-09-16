4db2acfa2d2d09162fce254ceab4954ab44da206
1SilentClap <44629862+HanQinDing@users.noreply.github.com>
2023-09-16 23:39:57 +0800

    Funcrion header

--- GoopEngine/Editor/FrameRateController/FrameRateController.h
+++ GoopEngine/Editor/FrameRateController/FrameRateController.h
@@ -4,16 +4,33 @@
 \date   $DAY_OF_MONTH-$MONTHNAME_EN-$YEAR
 \brief
 	Frame rate controller system.
-	Instructions:
 
-	1. Include this header file in your file
+
+	Setup:
+
+	1. // Include this header file in your files
 		 #include "../FrameRateController/FrameRateController.h"
 
-	2. Get an instance of the FrameRateController in your function/code:
+	2. // Initialize the FPS controller in your Game's Init (After Create Window)
+	   GE::FPS::FrameRateController* fps_control = &(GE::FPS::FrameRateController::GetInstance());
+		 fps_control->InitFrameRateController(60,1);
+
+	3. // Call the StartFrame() function at the start of each game loop ( Start of Update function )
+	   GE::FPS::FrameRateController::GetInstance().StartFrame();
+
+	4. // Call the EndFrame() function at the end of each game loop ( End of Draw function )
+	   GE::FPS::FrameRateController::GetInstance().EndFrame();
+
+ ------------------------------------------------------------------------------------------------	   
+
+	How to use:
+
+	1. Get an instance of the FrameRateController in your function/code:
 	   GE::FPS::FrameRateController* fps = &(GE::FPS::FrameRateController::GetInstance());
 
 	3. Access the function through the instance:
 	   double dt = fps->GetDeltaTime();
+
 
 Copyright (C) 2023 DigiPen Institute of Technology. All rights reserved.
 ************************************************************************/
@@ -41,18 +58,117 @@ namespace GE
 			
 
 		public:
-			void InitFrameRateController(int targetFPS, int fpsCalInterval);
-			void ResetFrameRateController();
-			int GetFrameCount();
-			double GetStartTime();
-			double GetPrevTime();
+
+
+			/*!*********************************************************************
+			\brief
+				.Function to Initialize the Frame Rate Controller. Call this in your Main init
+			\params
+				targetFPS 
+				FPS limit of your engine
+			\params
+				fpsCalInterval
+				Intervals between each FPS check (seconds)
+			************************************************************************/
+			void InitFrameRateController(int targetFPS, int fpsCalInterval = 1);
+
+
+			/*!*********************************************************************
+			\brief
+				.Function to get the delta time
+			\return
+				 Delta time (Amount of time pass since previous frame)
+			************************************************************************/
 			double GetDeltaTime();
-			double GetCurrTime();
+
+
+
+			/*!*********************************************************************
+			\brief
+				.Function to get the current FPS of the Engine
+			\return 
+				 Current FPS of the Engine
+			************************************************************************/
 			double GetFPS();
+
+
+
+			/*!*********************************************************************
+			\brief
+				.Function to Signfy the end of the current frame. Call this function before the end of each game loop
+			************************************************************************/
 			void EndFrame();
+
+
+			/*!*********************************************************************
+			\brief
+				.Function to Signfy the start of a new frame. call this at the start of each game loop
+			************************************************************************/
 			void StartFrame();
+
+
+			/*!*********************************************************************
+			\brief
+				.Function to get the current time
+			\return
+				 The current time
+			************************************************************************/
+			double GetCurrTime();
+
+			/*!*********************************************************************
+			\brief
+				.Function to get current frame's start time
+			\return
+				 starting time of current frame
+			************************************************************************/
+			double GetStartTime();
+
+
+			/*!*********************************************************************
+			\brief
+				.Function to get previous frame's start time
+			\return
+				 starting time of previous frame
+			************************************************************************/
+			double GetPrevTime();
+
+
+			/*!*********************************************************************
+			\brief
+				.Function to get the number frames passed since last check
+			\return
+				 Number of frames passed since the last time this function is called
+			************************************************************************/
+			int GetFrameCount();
+
+
+			/*!*********************************************************************
+			\brief
+				.Function to reset the FrameRateController
+			************************************************************************/
+			void ResetFrameRateController();
+
+			
+			/*!*********************************************************************
+			\brief
+				.Function to set the engine's FPS limit
+			\params
+				targetFPS
+				FPS limit of your engine
+			************************************************************************/
 			void SetTargetFPS(int targetFPS);
+
+
+
+			/*!*********************************************************************
+			\brief
+				.Function to set the engine's interval for FPS Check
+			\params
+				fpsCalInterval
+				Intervals between each FPS check (seconds)
+			************************************************************************/
 			void FPSCalInterval(int fpsCalInterval);
+
 
 		};
 	}
