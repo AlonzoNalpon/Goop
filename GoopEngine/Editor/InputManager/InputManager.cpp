31fa134df044608e848f520cff11e98d38108501
BryanWongCK <bryanwongchinkit@gmail.com>
2023-09-24 23:28:15 +0800

    Added ImGui callbacks to input manager

--- GoopEngine/Editor/InputManager/InputManager.cpp
+++ GoopEngine/Editor/InputManager/InputManager.cpp
@@ -1,4 +1,5 @@
 #include "InputManager.h"
+#include <ImGui/backends/imgui_impl_glfw.h>
 
 
 #define UNREFERENCED_PARAMETER(P) (P)
@@ -86,13 +87,23 @@ void InputManager::KeyCallback(GLFWwindow* window, int key, int scanCode, int ac
 {
 	UNREFERENCED_PARAMETER(scanCode);
 	UNREFERENCED_PARAMETER(mod);
-	m_keyReleased[key] = (GLFW_RELEASE == action);
-	m_keysTriggered[key] = (GLFW_PRESS == action);
+
+	ImGuiIO& io = ImGui::GetIO();
+	if (io.WantCaptureKeyboard)
+	{
+		ImGui_ImplGlfw_KeyCallback(window, key, scanCode, action, mod);
+	}
+
+	bool bit = !(GLFW_RELEASE == action);
+	m_keyReleased[key] = !bit;
+	m_keysTriggered[key] = bit;
 }
 
 // Mouse callback function
 void InputManager::MousePosCallback(GLFWwindow* window, double xpos, double ypos)
 {
+	ImGui_ImplGlfw_CursorPosCallback(window, xpos, ypos);
+
 	m_mousePos.x = xpos;
 	m_mousePos.y = ypos;
 
@@ -103,8 +114,16 @@ void InputManager::MouseButtonCallback(GLFWwindow* pwin, int button, int action,
 	UNREFERENCED_PARAMETER(pwin);
 	UNREFERENCED_PARAMETER(mod);
 
-	m_keyReleased[button] = (GLFW_RELEASE == action);
-	m_keysTriggered[button] = (GLFW_PRESS == action);
+	ImGuiIO& io = ImGui::GetIO();
+	bool bit = !(GLFW_RELEASE == action);
+
+	if (io.WantCaptureMouse)
+	{
+		ImGui_ImplGlfw_MouseButtonCallback(pwin, button, action, mod);
+	}
+
+	m_keyReleased[button] = !bit;
+	m_keysTriggered[button] = bit;
 
 }
 
@@ -113,6 +132,9 @@ void InputManager::MouseScrollCallback(GLFWwindow* pwin, double xoffset, double 
 	UNREFERENCED_PARAMETER(pwin);
 	UNREFERENCED_PARAMETER(xoffset);
 	UNREFERENCED_PARAMETER(yoffset);
+
+	ImGui_ImplGlfw_ScrollCallback(pwin, xoffset, yoffset);
+
 	//y_off = ((y_off + yoffset) > 4) ? 4 : ((y_off + yoffset) < -4) ? -4 : y_off + yoffset;
 	//std::cout << y_off << "\n";
 	////#ifdef _DEBUG
