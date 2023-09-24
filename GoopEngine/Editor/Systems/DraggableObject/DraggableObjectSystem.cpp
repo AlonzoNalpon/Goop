d86ee1b4a7e402e8445292192218dffb82f63f4f
BryanWongCK <bryanwongchinkit@gmail.com>
2023-09-24 18:42:24 +0800

    Re-arranged draggable logic
    Re-arrange checks to be more efficient.
    
    Todo: write better dragging positioning

--- GoopEngine/Editor/Systems/DraggableObject/DraggableObjectSystem.cpp
+++ GoopEngine/Editor/Systems/DraggableObject/DraggableObjectSystem.cpp
@@ -17,25 +17,25 @@ void DraggableObjectSystem::Update() {
 	{
 		//mouse click check
 		BoxCollider* entity1Col = m_ecs->GetComponent<BoxCollider>(entity);
-		Transform* transform = m_ecs->GetComponent<Transform>(entity);
-		Gravity* gravity = nullptr;
-		Velocity* velocity = nullptr;
 
-		if (m_ecs->GetComponent<Gravity>(entity) != NULL && m_ecs->GetComponent<Velocity>(entity) != NULL) {
-			gravity = m_ecs->GetComponent<Gravity>(entity);
-			velocity = m_ecs->GetComponent<Velocity>(entity);
-		}
-
-		dVec2 mousePos{};
-
-		InputManager* input = &(InputManager::GetInstance());
-		if (input->IsKeyHeld(GPK_MOUSE_LEFT))
+		if (entity1Col->m_mouseCollided)
 		{
-			mousePos = input->GetMousePosWorld();
-			if (entity1Col->m_mouseCollided)
+			Transform* transform = m_ecs->GetComponent<Transform>(entity);
+			Gravity* gravity = m_ecs->GetComponent<Gravity>(entity, true);
+			Velocity* velocity = m_ecs->GetComponent<Velocity>(entity, true);
+
+			dVec2 mousePos{};
+			InputManager* input = &(InputManager::GetInstance());
+			if (input->IsKeyHeld(GPK_MOUSE_LEFT))
 			{
-				gravity->SetActive(false);
-				velocity->SetActive(false);
+				if (gravity)
+				{
+					gravity->SetActive(false);
+				}
+				if (velocity)
+				{
+					velocity->SetActive(false);
+				}
 
 				dVec2 prevObjPos = transform->m_pos;
 				dVec2 currMousePos = mousePos;
@@ -49,13 +49,51 @@ void DraggableObjectSystem::Update() {
 
 				transform->m_pos = prevObjPos + distTravelled;
 			}
+			else if (input->IsKeyReleased(GPK_MOUSE_LEFT))
+			{
+				if (gravity)
+				{
+					gravity->SetActive(true);
+				}
+				if (velocity)
+				{
+					velocity->SetActive(true);
+				}
+			}
 		}
 
-		if (!input->IsKeyHeld(GPK_MOUSE_LEFT)) {
-			gravity->SetActive(true);
-			velocity->SetActive(true);
-		}
+		//Transform* transform = m_ecs->GetComponent<Transform>(entity);
+		//Gravity* gravity = m_ecs->GetComponent<Gravity>(entity);
+		//Velocity* velocity = m_ecs->GetComponent<Velocity>(entity);
 
-		entity1Col->Render();
+		//dVec2 mousePos{};
+
+		//InputManager* input = &(InputManager::GetInstance());
+		//if (input->IsKeyHeld(GPK_MOUSE_LEFT))
+		//{
+		//	mousePos = input->GetMousePosWorld();
+		//	if (entity1Col->m_mouseCollided)
+		//	{
+		//		gravity->SetActive(false);
+		//		velocity->SetActive(false);
+
+		//		dVec2 prevObjPos = transform->m_pos;
+		//		dVec2 currMousePos = mousePos;
+		//		dVec2 newMousePos = input->GetMousePosWorld();
+		//		dVec2 distTravelled = newMousePos - currMousePos;
+
+		//		std::cout << "prevObjPos: " << prevObjPos << std::endl;
+		//		std::cout << "currMousePos: " << currMousePos << std::endl;
+		//		std::cout << "newMousePos: " << newMousePos << std::endl;
+		//		std::cout << "distTravelled: " << distTravelled << std::endl;
+
+		//		transform->m_pos = prevObjPos + distTravelled;
+		//	}
+		//}
+		//else
+		//{
+		//	gravity->SetActive(true);
+		//	velocity->SetActive(true);
+		//}
 	}
 }
