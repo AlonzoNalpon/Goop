9ab189da9807f9f46d0005f95941f0b19749de3c
BearCustomerService <a.nalpon@digipen.edu>
2023-09-27 11:56:32 +0800

    Separated animation system from rendering system

--- GoopEngine/Editor/Systems/SpriteAnim/SpriteAnimSystem.cpp
+++ GoopEngine/Editor/Systems/SpriteAnim/SpriteAnimSystem.cpp
@@ -7,4 +7,45 @@ namespace GE::Systems
 {
   constexpr double pi = 3.14159265358979323846;
 
+  void SpriteAnimSystem::Update()
+  {
+    Graphics::GraphicsEngine& gEngine{ Graphics::GraphicsEngine::GetInstance() };
+    double dt{ FPS::FrameRateController::GetInstance().GetDeltaTime() };
+    f32 dtF{ static_cast<f32>(dt) };
+    for (GE::ECS::Entity entity : m_entities)
+    {
+      //Component::Model* model{ m_ecs->GetComponent<Component::Model>(entity) };
+      Component::Sprite* sprite{ m_ecs->GetComponent<Component::Sprite>(entity) };
+      Component::SpriteAnim* animData{ m_ecs->GetComponent<Component::SpriteAnim>(entity) };
+      // Updating the sprite anim data (if any)
+      if (animData)
+      {
+        // get the sprite animation
+        Graphics::SpriteAnimation const& spriteAnim
+        { gEngine.animManager.GetAnim(animData->animID) };
+
+        // Updating sprite animation data
+        {
+          // Update the timer and see if frame should be changed
+          animData->currTime += dt;
+          if (animData->currTime >= spriteAnim.speed)
+          {
+            animData->currTime -= spriteAnim.speed; // reset speed for next frame
+            // move to next frame, or wrap back to start
+            u32 const newFrame{ (animData->currFrame + 1) };
+            animData->currFrame = (newFrame >= spriteAnim.frames.size() ?
+              0 : newFrame);
+          }
+        }
+
+        // setting the new sprite based on data
+        sprite->spriteData.info = spriteAnim.frames[animData->currFrame];
+      }
+    }
+  }
+
+  void SpriteAnimSystem::OnDestroyed()
+  {
+  }
+
 }
--- GoopEngine/Editor/Systems/SpriteAnim/SpriteAnimSystem.h
+++ GoopEngine/Editor/Systems/SpriteAnim/SpriteAnimSystem.h
@@ -4,6 +4,7 @@
 #include <iostream>
 
 // The involved components:
+#include <Component/Sprite.h>			// sprite (ALL MUST HAVE THIS)
 #include <Component/SpriteAnim.h> // optional and will edit the sprite component
 namespace GE::Systems {
   class SpriteAnimSystem : public GE::ECS::System
