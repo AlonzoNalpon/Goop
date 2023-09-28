1adf51d6753d1e8de46edaf68a90d199d74e2529
BearCustomerService <a.nalpon@digipen.edu>
2023-09-28 19:34:45 +0800

    added new file for debug render lines struct

---
+++ GoopEngine/Editor/Graphics/Renderer/DebugRenderData.h
@@ -0,0 +1,12 @@
+#ifndef DEBUG_RENDER_DATA
+#define DEBUG_RENDER_DATA
+#include <Graphics/Def/GraphicsTypes.h>
+namespace Graphics::Rendering
+{
+  struct DLineRenderData
+  {
+    gVec2 startPt;
+    gVec2 endPt;
+  };
+}
+#endif
