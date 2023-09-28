5c1ab63408169b62d49ccd50ccfed2bc59f86d42
joellohki <110614623+joellohki@users.noreply.github.com>
2023-09-28 21:52:41 +0800

    Commenting of code and splitting of my classes

--- GoopEngine/Editor/ObjectFactory/ObjectStructs.h
+++ GoopEngine/Editor/ObjectFactory/ObjectStructs.h
@@ -1,3 +1,12 @@
+/*!*********************************************************************
+\file   ObjectStructs.h
+\author loh.j@digipen.edu
+\date   28 September 2023
+\brief
+  Contains the structs required for ObjectFactory.cpp
+
+Copyright (C) 2023 DigiPen Institute of Technology. All rights reserved.
+************************************************************************/
 #pragma once
 #include "../ECS/SystemTypes.h"
 
@@ -5,11 +14,8 @@ namespace GE
 {
   namespace ObjectFactory
   {
-    
-
     using ComponentMap = std::unordered_map<GE::ECS::COMPONENT_TYPES, std::string>;
     using SystemMap = std::unordered_map<GE::ECS::SYSTEM_TYPES, std::string>;
-    
 
     struct PrefabData
     {
@@ -23,7 +29,5 @@ namespace GE
       GE::ECS::ComponentSignature m_componentSignature;
       ComponentMap m_components;
     };
-
-    
   }
 }
