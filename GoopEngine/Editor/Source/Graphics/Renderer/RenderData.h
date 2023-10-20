3e788c2b61a6e5973d70d3d36c8cf27a5e1804d2
BearCustomerService <a.nalpon@digipen.edu>
2023-10-21 00:11:36 +0800

    now rendering with transform from math library!

--- GoopEngine/Editor/Source/Graphics/Renderer/RenderData.h
+++ GoopEngine/Editor/Source/Graphics/Renderer/RenderData.h
@@ -21,13 +21,18 @@ namespace Graphics::Rendering
    */
   struct Transform
   {
-    gVec3     scale;
-    GLfloat   rotation;
-    gVec3     pos;
-    Transform(GLfloat scale, GLfloat rot, gVec3 pos) : 
-      scale{ scale, scale, scale }, rotation{ rot }, pos{pos} {}
-    Transform(gVec3 const&scale, GLfloat rot, gVec3 const&pos) :
-      scale{ scale }, rotation{ rot }, pos{ pos } {}
+    //gVec3     scale;      // deprecated
+    //GLfloat   rotation;   // deprecated
+    //gVec3     pos;        // deprecated
+
+    //glm::mat4 mat;
+
+    //Transform(GLfloat scale, GLfloat rot, gVec3 pos) : 
+    //  scale{ scale, scale, scale }, rotation{ rot }, pos{pos} {}
+    //Transform(gVec3 const&scale, GLfloat rot, gVec3 const&pos) :
+    //  scale{ scale }, rotation{ rot }, pos{ pos } {}
+
+
   };
 
 
@@ -40,13 +45,13 @@ namespace Graphics::Rendering
   {
     gObjID mdl;         //!< model data (ALL MODELS MUST USE THIS). It has shader stored within
     SpriteData sprite;  //!< sprite data handle (optional)
-    Transform transform;//!< the transformation of the model
+    glm::mat4 transform;//!< the transformation of the model
   };
 
 
   struct DepthComp {
     bool operator()(RenderData const& a, RenderData const& b)const {
-      return a.transform.pos.z > b.transform.pos.z;
+      return a.transform[3][2] > b.transform[3][2];
     }
   };
 };
