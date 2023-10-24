4f534faf1cc21be73131750bc982678a39b0e0d9
jiaaxenn <chrisxphuaxje@gmail.com>
2023-10-24 13:22:53 +0800

    created AssetBrowser files

---
+++ GoopEngine/Editor/Source/EditorUI/AssetBrowser.cpp
@@ -0,0 +1,12 @@
+#include <pch.h>
+#include "AssetBrowser.h"
+
+void GE::EditorGUI::AssetBrowser::CreateContent()
+{
+
+}
+
+void GE::EditorGUI::AssetBrowser::Propergate(std::string filename, ImColor textClr)
+{
+
+}
