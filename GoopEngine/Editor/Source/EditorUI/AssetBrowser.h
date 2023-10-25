acfa4ed40e4329b1b6d9a2a31652e694b933b011
jiaaxenn <chrisxphuaxje@gmail.com>
2023-10-25 13:10:10 +0800

    implemented asset view
    implemented asset view
    
    - error when trying to load images when there is no .png files

--- GoopEngine/Editor/Source/EditorUI/AssetBrowser.h
+++ GoopEngine/Editor/Source/EditorUI/AssetBrowser.h
@@ -1,12 +1,20 @@
 #pragma once
 #include "ImGuiUI.h"
+#include <ImGui/imgui.h>
 
 namespace GE::EditorGUI
 {
 	class AssetBrowser
 	{
+	private:
+		static std::set<ImTextureID> m_textID;
+		static std::set<int> m_assetIDs;
+
+		static void Traverse(std::filesystem::path filepath, ImColor textClr);
+
 	public:
 		static void CreateContentDir();
 		static void CreateContentView();
+		static void InitView();
 	};
 }
--- GoopEngine/Editor/Source/EditorUI/ImGuiUI.cpp
+++ GoopEngine/Editor/Source/EditorUI/ImGuiUI.cpp
@@ -238,6 +238,10 @@ void ImGuiUI::Update()
   AssetBrowser::CreateContentDir();
   End();
 
+  Begin("Asset View");
+  AssetBrowser::CreateContentView();
+  End();
+
   
   if (Visualizer::IsPerformanceShown())
   {
