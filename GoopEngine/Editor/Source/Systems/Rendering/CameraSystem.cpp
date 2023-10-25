325bc261c8dd86c96cd4f20e798f59eed49ab288
BearCustomerService <a.nalpon@digipen.edu>
2023-10-25 11:47:01 +0800

    Starting resources for camera system

---
+++ GoopEngine/Editor/Source/Systems/Rendering/CameraSystem.cpp
@@ -0,0 +1,18 @@
+#include <pch.h>
+#include <Systems/Rendering/CameraSystem.h>
+#include <Graphics/GraphicsEngine.h>
+
+namespace GE::Systems
+{
+  void CameraSystem::Update()
+  {
+    Graphics::GraphicsEngine& gEngine{ Graphics::GraphicsEngine::GetInstance() };
+    for (GE::ECS::Entity entity : GetUpdatableEntities())
+    {
+      
+    }
+  }
+  void CameraSystem::OnDestroyed()
+  {
+  }
+}
