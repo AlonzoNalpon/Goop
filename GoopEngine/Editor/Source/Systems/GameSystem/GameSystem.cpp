adda8c91b2256f3f13bef3f2f0a2f54724506d71
BryanWongCK <bryanwongchinkit@gmail.com>
2023-11-11 16:02:21 +0800

    More gamesystem staging

--- GoopEngine/Editor/Source/Systems/GameSystem/GameSystem.cpp
+++ GoopEngine/Editor/Source/Systems/GameSystem/GameSystem.cpp
@@ -1,11 +1,23 @@
 #include <pch.h>
 #include "GameSystem.h"
+#include <Component/Game.h>
+#include <Component/ScriptHandler.h>
+
+using namespace GE::Component;
+using namespace GE::ECS;
 
 void GE::Systems::GameSystem::Update()
 {
-  for (GE::ECS::Entity entity : GetUpdatableEntities())
+  for (Entity entity : GetUpdatableEntities())
   {
+    Game* game = m_ecs->GetComponent<Game>(entity);
+    Entity player = game->player;
+    Entity enemy = game->enemy;
     // Pass player and enemy entity stats to 
     // core game script
+
+    ScriptHandler* sh = m_ecs->GetComponent<GE::Component::ScriptHandler>(entity);
+    // Invoke mono method passing in player and enemy data
+    
   }
 }
