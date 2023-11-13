30d762226170ea2c04d1ecc2e8dd7eb2cacbce91
BryanWongCK <bryanwongchinkit@gmail.com>
2023-11-13 18:36:48 +0800

    Added cards and "drawing"

--- GoopEngine/GoopScripts/Gameplay/StateMachine.cs
+++ GoopEngine/GoopScripts/Gameplay/StateMachine.cs
@@ -18,16 +18,41 @@ namespace GoopScripts
       TOTAL_PHASES
     }
 
+    public double timeBeforeAction;
+    private double currTime;
     public Phase currPhase;
+    Random rng;
 
-    public void NextPhase()
+    StateMachine()
     {
-      currPhase = (Phase)((int)(currPhase + 1) % (int)Phase.TOTAL_PHASES);
+      timeBeforeAction = 0;
+      currTime = 0;
+      currPhase = Phase.START;
+      rng = new Random();
     }
 
-    public void Update(PlayerStats playerStats, EnemyStats enemyStats, double dt)
-    {
+    public void NextPhase()
+		{
+			// Should indicate to c++ to do next phase animations
+			currPhase = (Phase)((int)(currPhase + 1) % (int)Phase.TOTAL_PHASES);
+    }
 
+    public void Update(Stats playerStats, Stats enemyStats, double dt)
+    {
+      currTime -= dt;
+
+      if (currTime < 0)
+			{
+				currTime = timeBeforeAction;
+
+        NextPhase();
+        // Play cards in queue
+        playerStats.cardQueue.First();
+
+        // Everyone add 1 card to queue from deck
+        playerStats.cardQueue.Add(playerStats.deck[rng.Next(0, playerStats.deck.Count() - 1)]);
+				enemyStats.cardQueue.Add(enemyStats.deck[rng.Next(0, enemyStats.deck.Count() - 1)]);
+			}
     }
   }
 }
--- GoopEngine/GoopScripts/Gameplay/Stats.cs
+++ GoopEngine/GoopScripts/Gameplay/Stats.cs
@@ -8,5 +8,10 @@ namespace GoopScripts
 {
 	internal abstract class Stats
 	{
+		public uint health;
+		// List of card IDs
+		public List<uint> deck;
+		// List of cards in the queue
+		public List<uint> cardQueue;
 	}
 }
