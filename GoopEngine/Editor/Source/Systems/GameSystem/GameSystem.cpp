0816354fc41a3f635c482a7b5e42b89cc6203a34
BryanWongCK <bryanwongchinkit@gmail.com>
2023-11-13 19:37:56 +0800

    Pass mono object no mono class

--- GoopEngine/Editor/Source/Systems/GameSystem/GameSystem.cpp
+++ GoopEngine/Editor/Source/Systems/GameSystem/GameSystem.cpp
@@ -17,13 +17,10 @@ void GE::Systems::GameSystem::Update()
     //Entity player = game->player;
     //Entity enemy = game->enemy;
 
-    MonoClass* playerScriptObj = mono_object_get_class(game->playerScript.m_classObjInst);
-    MonoClass* enemyScriptObj = mono_object_get_class(game->enemyScript.m_classObjInst);
     MonoClass* gameSystemScriptObj = mono_object_get_class(game->gameSystemScript.m_classObjInst);
-
     MonoMethod* update = mono_class_get_method_from_name(gameSystemScriptObj, "Update", 3);
     double dt = frc.GetDeltaTime();
-    std::vector<void*> args{ &playerScriptObj, &enemyScriptObj, &dt };
+    std::vector<void*> args{ &game->playerScript.m_classObjInst, & game->enemyScript.m_classObjInst, &dt };
     mono_runtime_invoke(update, gameSystemScriptObj, args.data(), nullptr);
   }
 }
