10019d824a4b6a679072e16e541ac275fe0cb487
jiaaxenn <chrisxphuaxje@gmail.com>
2024-01-31 12:29:38 +0800

    updated card scripts

--- GoopEngine/GoopScripts/Cards/SpecialChargeUp.cs
+++ GoopEngine/GoopScripts/Cards/SpecialChargeUp.cs
@@ -40,6 +40,7 @@ namespace GoopScripts.Cards
     ************************************************************************/
     public override void Play(ref Stats source, ref Stats target)
     {
+      source.m_buffs.AddBuff(new Buff(Buff.BuffType.SKIP_TURN, 0.0f, 1, "Skipped"));
       source.m_buffs.AddBuff(new Buff(Buff.BuffType.INCREASE_ATK_DEALT, 2.0f, 2, "Atk Up"));
     }
   }
--- GoopEngine/GoopScripts/Cards/SpecialSmokeScreen.cs
+++ GoopEngine/GoopScripts/Cards/SpecialSmokeScreen.cs
@@ -40,7 +40,7 @@ namespace GoopScripts.Cards
     ************************************************************************/
     public override void Play(ref Stats source, ref Stats target)
     {
-      source.m_buffs.AddBuff(new Buff(Buff.BuffType.IMMUNITY, 0.0f, 2, "Dmg Down"));
+      source.m_buffs.AddBuff(new Buff(Buff.BuffType.MULTIPLY_ATK_DEALT, 0.0f, 2, "Dmg Down"));
     }
   }
 }
