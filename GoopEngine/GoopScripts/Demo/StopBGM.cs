948fc2f48218304c6e1ce8867a9c889002f71314
BryanWongCK <bryanwongchinkit@gmail.com>
2024-02-04 00:50:21 +0800

    Demo Scene Setup

---
+++ GoopEngine/GoopScripts/Demo/StopBGM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoopScripts.Button;
+using GoopScripts.Mono;
+
+namespace GoopScripts.Demo
+{
+	internal class StopBGM : IButtonClick
+	{
+		public void OnClick(uint entity)
+		{
+			Utils.StopChannel(Utils.ChannelType.BGM);
+		}
+	}
+}
