5195f468d2ec201a6175b2b654adda8ddcd0fe13
BearCustomerService <a.nalpon@digipen.edu>
2024-02-04 00:48:39 +0800

    Updated anim sprite event system

--- GoopEngine/Editor/Source/Systems/AnimEvent/AnimEventsSystem.cpp
+++ GoopEngine/Editor/Source/Systems/AnimEvent/AnimEventsSystem.cpp
@@ -1,5 +1,8 @@
 #include <pch.h>
 #include <Systems/AnimEvent/AnimEventsSystem.h>
+#include <Graphics/GraphicsEngine.h>
+#include <SpriteAnimation/SpriteAnimationManager.h>
+#include <Events/AnimEventManager.h>
 namespace GE::Systems
 {
   void AnimEventsSystem::Update()
@@ -8,10 +11,38 @@ namespace GE::Systems
     {
       Component::AnimEvents* animEvents{ m_ecs->GetComponent<Component::AnimEvents>(entity) };
       Component::SpriteAnim* spriteAnim{ m_ecs->GetComponent<Component::SpriteAnim>(entity) };
-      if (!animEvents || spriteAnim)
+      auto const& animMan{ Graphics::GraphicsEngine::GetInstance().animManager };
+      if (!animEvents || !spriteAnim) // skip if the event/anim components were missing
         continue;
 
-      spriteAnim->currFrame;
+      auto const& eventTable{ Events::AnimEventManager::GetInstance().GetAnimEventsTable() };
+      for (auto const& currEventPack : animEvents->m_eventList)
+      {
+        if (currEventPack.first == spriteAnim->animID) // if the animation matches this event set
+        {
+          // We would check if current frame has events
+          auto tableIt{ eventTable.find(currEventPack.second) }; // Get the matching events entry iterator
+          if (tableIt == eventTable.end())
+          {
+            // Log error?
+            continue;
+          }
+          // Get the iterator to container for all possible events in this sprite
+          Component::AnimEvents::AnimEventsCont const& currEvtCont = tableIt->second;
+          auto eventsIt = currEvtCont.find(spriteAnim->currFrame); // then we get all events in this frame (if any)
+          if (eventsIt == currEvtCont.end())
+            continue; // no events in this frame, skip!
+
+          // Initialize reference to container for all events in this frame
+          Component::AnimEvents::AnimFrameEvents const& currFrameEvents = eventsIt->second;
+
+          for (std::string const& evtScript : currFrameEvents)
+          {
+            evtScript; // THIS IS THE SCRIPT NAME
+          }
+        }
+      }
+      
     }
   }
 }
