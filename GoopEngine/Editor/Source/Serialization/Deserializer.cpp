285658e8183befbbb2ec45af438e448e32df0e6a
Cheng En <chengen.lau@digipen.edu>
2024-02-05 12:42:32 +0800

    Comment deserializer debug flag

--- GoopEngine/Editor/Source/Serialization/Deserializer.cpp
+++ GoopEngine/Editor/Source/Serialization/Deserializer.cpp
@@ -22,7 +22,7 @@ Copyright (C) 2023 DigiPen Institute of Technology. All rights reserved.
 #include <Prefabs/PrefabManager.h>
 
 #ifdef _DEBUG
-#define DESERIALIZER_DEBUG
+//#define DESERIALIZER_DEBUG
 #endif
 
 using namespace GE;
