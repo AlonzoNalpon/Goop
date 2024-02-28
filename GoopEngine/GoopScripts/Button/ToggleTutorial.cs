14b3f0bd30222baed2753efa398f33da315ab6f8
jiaaxenn <chrisxphuaxje@gmail.com>
2024-02-28 18:50:46 +0800

    toggle tutorial button script

--- GoopEngine/GoopScripts/Button/ToggleTutorial.cs
+++ GoopEngine/GoopScripts/Button/ToggleTutorial.cs
@@ -9,12 +9,28 @@ namespace GoopScripts.Button
 {
   internal class ToggleTutorial : IButtonClick
   {
+    int m_tut;
+
+    ToggleTutorial()
+    {
+      m_tut = 1;
+    }
     public void OnClick(uint entity)
     {
-      //Utils.SetIsActiveEntity(Utils.GetEntity($"Cutscenes_00{lastFrame}"), false);
-      //Utils.SetIsActiveEntity(Utils.GetEntity($"Cutscenes_00{frame}"), true);
+      int m_prev = m_tut++;
 
+      //Utils.SetIsActiveEntity(Utils.GetEntity($"Tutorial_{m_prev}"), false);
+      //Utils.SetIsActiveEntity(Utils.GetEntity($"Tutorial_{m_tut}"), true);
+    }
 
+    public int GetTutorial()
+    {
+      return m_tut;
+    }
+
+    public int IncTutorial()
+    {
+      return ++m_tut;
     }
   }
 }
