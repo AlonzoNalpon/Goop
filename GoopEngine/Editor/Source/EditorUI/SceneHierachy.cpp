02e218b6fd08e5e65f46e7884b7b73797d8143a5
Cheng En <chengen.lau@digipen.edu>
2024-03-09 14:13:53 +0800

    You can now rename prefabs when creating one from scene hierarchy

--- GoopEngine/Editor/Source/EditorUI/SceneHierachy.cpp
+++ GoopEngine/Editor/Source/EditorUI/SceneHierachy.cpp
@@ -11,6 +11,7 @@ Copyright (C) 2023 DigiPen Institute of Technology. All rights reserved.
 #ifndef IMGUI_DISABLE
 #include "SceneHierachy.h"
 #include <ImGui/imgui.h>
+#include "../ImGui/misc/cpp/imgui_stdlib.h"
 #include <Component/Transform.h>
 #include <ObjectFactory/ObjectFactory.h>
 #include <GameStateManager/GameStateManager.h>
@@ -42,7 +43,8 @@ namespace
 
 	std::vector<Entity> entitiesToDestroy;
 
-	bool treeNodePopUp{ false };
+	bool treeNodePopUp{ false }, prefabPopup{ false };
+	GE::ECS::Entity selectedEntity;	// for prefab popup
 
 	/*!*********************************************************************
 	\brief 
@@ -77,6 +79,12 @@ namespace
 	********************************************************************/
 	void Propergate(Entity entity, EntityComponentSystem& ecs, ImGuiTreeNodeFlags flag, ImColor textClr);
 
+	/*!*********************************************************************
+	\brief
+		Runs the code for the popup when "Create Prefab" option has been
+		selected from the scene hierarchy
+	************************************************************************/
+	void RunCreatePrefabPopup();
 }
 
 void GE::EditorGUI::SceneHierachy::CreateContent()
@@ -261,7 +269,8 @@ namespace
 				ImGui::BeginDisabled(GE::EditorGUI::PrefabEditor::IsEditingPrefab());
 				if (Selectable("Save as Prefab"))
 				{
-					GE::Prefabs::PrefabManager::GetInstance().CreatePrefabFromEntity(entity, ecs.GetEntityName(entity));
+					prefabPopup = true;
+					selectedEntity = entity;
 				}
 				ImGui::EndDisabled();
 
@@ -374,6 +383,72 @@ namespace
 			{
 				GE::EditorGUI::ImGuiHelper::SetSelectedEntity(entity);
 			}
+		}
+
+		if (prefabPopup)
+		{
+			ImGui::OpenPopup("Create Prefab");
+			prefabPopup = false;
+		}
+		RunCreatePrefabPopup();
+	}
+
+	void RunCreatePrefabPopup()
+	{
+		ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+		if (ImGui::BeginPopupModal("Create Prefab", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+		{
+			static std::string prefabName{};
+			static bool blankWarning{ false }, existingPrefabWarning{ false };
+
+			if (blankWarning)
+			{
+				ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "Name cannot be blank!!!");
+			}
+			else if (existingPrefabWarning)
+			{
+				ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "Prefab already exists!");
+			}
+
+			ImGui::Text("Name of Prefab:");
+			ImGui::SameLine();
+			if (ImGui::InputText("##PrefabNameInput", &prefabName))
+			{
+				blankWarning = existingPrefabWarning = false;
+			}
+
+			ImGui::SetCursorPosX(0.5f * (ImGui::GetWindowContentRegionMax().x - ImGui::CalcTextSize("Cancel Create ").x));
+			if (ImGui::Button("Cancel"))
+			{
+				prefabName.clear();
+				blankWarning = existingPrefabWarning = false;
+				ImGui::CloseCurrentPopup();
+			}
+
+			ImGui::SameLine();
+			if (ImGui::Button("Create"))
+			{
+				// if name is blank / whitespace, reject it
+				if (prefabName.find_first_not_of(" ") == std::string::npos)
+				{
+					blankWarning = true;
+					existingPrefabWarning = false;
+				}
+				else if (GE::Prefabs::PrefabManager::GetInstance().DoesPrefabExist(prefabName))
+				{
+					existingPrefabWarning = true;
+					blankWarning = false;
+				}
+				else
+				{
+					GE::Prefabs::PrefabManager::GetInstance().CreatePrefabFromEntity(selectedEntity, prefabName);
+					blankWarning = existingPrefabWarning = false;
+					prefabName.clear();
+					ImGui::CloseCurrentPopup();
+				}
+			}
+
+			ImGui::EndPopup();
 		}
 	}
 }
