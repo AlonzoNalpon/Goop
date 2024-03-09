40b651f8213219f78ba99a673403a390db4cf3e6
BryanWongCK <bryanwongchinkit@gmail.com>
2024-03-09 16:37:42 +0800

    Transition prefab and updated options

---
+++ GoopEngine/GoopScripts/Button/TransitionOut.cs
@@ -0,0 +1,35 @@
+﻿/*!*********************************************************************
+\file   TransitionOut.cs 
+\author w.chinkitbryam\@digipen.edu
+\date   09 March 2024
+\brief  
+	Triggers the transition animation
+ 
+Copyright (C) 2023 DigiPen Institute of Technology. All rights reserved.
+************************************************************************/
+using GoopScripts.Mono;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoopScripts.Button
+{
+	public class TransitionOut : TextButtonBase
+	{
+		/*!******************************************************************
+		\brief
+			Callback for when mouse is released after clicking the entity
+		
+		\param entity
+			Entity ID of the entity calling the event
+		********************************************************************/
+		public override void OnRelease(uint entity)
+		{
+			base.OnRelease(entity);			
+			Utils.PlayTransformAnimation(Utils.GetEntity("TransitionOut"), "TransitionOut");
+		}
+	}
+}
