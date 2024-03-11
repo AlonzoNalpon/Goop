882581ad8918c1f8f7dd70c3cdb52f2b030ac70b
BryanWongCK <bryanwongchinkit@gmail.com>
2024-03-11 10:59:10 +0800

    Update ContinueTutorial.cs
    Using new text button base

--- GoopEngine/GoopScripts/Source/Button/Tutorial/ContinueTutorial.cs
+++ GoopEngine/GoopScripts/Source/Button/Tutorial/ContinueTutorial.cs
@@ -8,10 +8,11 @@ using System.Threading.Tasks;
 
 namespace GoopScripts.Button
 {
-  internal class ContinueTutorial : IButtonClick
+  internal class ContinueTutorial : TextButtonBase
   {
-    public void OnClick(uint entity)
+    public override void OnRelease(uint entity)
     {
+      base.OnRelease(entity);
       Utils.SetIsActiveEntity(Utils.GetEntity("Skip_Tutorial_Prompt"), false);
       Utils.SetIsActiveEntity(Utils.GetEntity("SkipButton"), true);
       Utils.SetIsActiveEntity(Utils.GetEntity($"Tutorial_{Tutorial.m_tut}"), true);
