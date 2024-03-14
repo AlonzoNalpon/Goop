1645e3ad4a4755399d1fc8a72031c036936e21ab
BearCustomerService <a.nalpon@digipen.edu>
2024-03-14 13:09:50 +0800

    combopage script

---
+++ GoopEngine/GoopScripts/Source/Gameplay/ComboPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoopScripts.Mono;
+using static GoopScripts.Mono.Utils;
+
+namespace GoopScripts.Source.Gameplay
+{
+    internal class ComboPage : Entity
+    {
+        public void OnCreate()
+        {
+        }
+
+        public void OnUpdate(double deltaTime)
+        {
+        }
+    }
+}
