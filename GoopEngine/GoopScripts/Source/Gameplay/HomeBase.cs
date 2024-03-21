a92fd9f15b2fe8659272c09cf86d8b0570b28c74
BryanWongCK <bryanwongchinkit@gmail.com>
2024-03-21 10:43:07 +0800

    optimized homebase.cs

--- GoopEngine/GoopScripts/Source/Gameplay/HomeBase.cs
+++ GoopEngine/GoopScripts/Source/Gameplay/HomeBase.cs
@@ -23,62 +23,20 @@ namespace GoopScripts.Gameplay
   {
     static int m_levelToLoad = 0;
 
-    List<CardBase.CardID> m_tempDeck = new List<CardBase.CardID>();
-    static Dictionary<CardBase.CardID, int> m_cards = new Dictionary<CardBase.CardID, int>();
     public string playerSavePath;
     public int[] CardIDs = new int[6];
-
-    public HomeBase()
-    {
-      
-    }
 
     public void OnCreate()
     {
       Serialization.SerialReader.IncrementLevel("./Assets/GameData/PlayerStats.sav");
-      m_tempDeck.Clear();
-      m_cards.Clear();
       var statsInfo = Serialization.SerialReader.LoadPlayerState(playerSavePath);
       m_levelToLoad = statsInfo.levelToLoad;
       Console.WriteLine(m_levelToLoad);
+      int counter = 0;
       foreach (var card in statsInfo.deckList)
       {
-        for (int i = 0; i < card.Item2; ++i)
-        {
-          m_tempDeck.Add(card.Item1);
-          // Console.Write(card.Item1);
-        }
+        Utils.SetTextComponent((int)Utils.GetChildEntity((uint)CardIDs[counter++], "Text"), "x" + card.Item2);
       }
-      GetUniqueCards(m_tempDeck);
-      for (int i = 0; i < CardIDs.Length; ++i)
-      {
-        Utils.SetTextComponent((int)Utils.GetChildEntity((uint)CardIDs[i], "Text"), "x" + HomeBase.GetCount(CardBase.CardID.LEAH_BEAM + i));
-      }
-    }
-
-    public void GetUniqueCards(List<CardBase.CardID> deck)
-    {
-      for (CardBase.CardID i = CardBase.CardID.LEAH_BEAM; i <= CardBase.CardID.SPECIAL_RAGE; ++i)
-      {
-        m_cards.Add(i, 0);
-      }
-
-      for (int i = 0; i < deck.Count; ++i)
-      {
-        if (m_cards.ContainsKey(deck[i]))
-        {
-          ++m_cards[deck[i]];
-        }
-      }
-    }
-
-    static public int GetCount(CardBase.CardID card)
-    {
-      if (m_cards.ContainsKey(card))
-      {
-        return m_cards[card];
-      }
-      return 0;
     }
 
     static public int GetLevelToLoad()
